d4f17b1dd0a685c068c8e11f5257c9cee5919a20
Cristian2332 <156873105+Cristian2332@users.noreply.github.com>
2024-02-21 17:11:44 -0600

    Create programa02.py

---
+++ programa02.py
@@ -0,0 +1,14 @@
+"""
+Nombre: Cristian Peralta Aviles
+Matricula: 1722110292
+Grupo: TI21
+Fecha: 18/01/2024
+Aqui se aprendio a invocar al constructor de una clase
+"""
+class A: #Define la clase
+    matricula = None #Define la variable matricula
+    nombre = None #Define la variable nombre
+    def __init__(self): #Define el constructor
+        print("Constructor de la clase A") #Imprime los datos
+
+objetoA=A() #Crea un objeto de la clase a
