2e9d7f362fd8ea2c2c737a00b13cd464b6767ef0
Cristian2332 <156873105+Cristian2332@users.noreply.github.com>
2024-02-21 17:12:12 -0600

    Create programa03.py

---
+++ programa03.py
@@ -0,0 +1,14 @@
+Nombre: Cristian Peralta Aviles
+Matricula: 1722110292
+Grupo: TI21
+Fecha: 18/01/2024
+Es un codigo con errores
+"""
+class Alumnos: #Define la clase
+    matricula = None #Define la variable
+    nombre = None #Define la variable
+    def __init__(self, matricula, nombre): #Define al constructor
+        self.matricula = matricula #Da valor a la variable
+        self.nombre = nombre #Da valor a la variable
+        print("Objeto Alumnos") #Imprime el texto
+objetoAlumnos = Alumnos() #Crea el objeto
