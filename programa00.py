3b932bb4c172af80726834b99ca56c1d4174a0fd
Cristian2332 <156873105+Cristian2332@users.noreply.github.com>
2024-02-21 17:09:05 -0600

    Update programa00.py

--- programa00.py
+++ programa00.py
@@ -1,21 +1,8 @@
+"""
 Nombre: Cristian Peralta Aviles
 Matricula: 1722110292
 Grupo: TI21
 Fecha: 18/01/2024
-En este codigo aprendimos a como realizar una suma, y unos mensajes en python
+En este apartado aprendimos a utilizar la funcion print que nos ayuda a imprimir lo que tengamos
 """
-class Alumno: #Define la clase
-  matricula = None #Declara la variable matricula
-  nombre = None #Declara la variable nombre
-  def __init__(self, matricula, nombre): #Define el constructor
-      self.matricula = matricula #Asigna el valor de matricula
-      self.nombre = nombre #Asigna el valor de nombre
-      print("Objeto Alumnos") #Imprime el mensaje
-  def estudiar(self): #Define el metodo estudiar
-      print(f"{self.nombre}estudia") #Imprime el mensaje
-  def sumar(self, numero1, numero2): #Define el metodo sumar
-      suma = numero1 + numero1 #Asigna el valor de la suma
-      print(f"{numero1}+{numero2}={suma}") #Imprime el mensaje
-dejah = Alumno(1111,"Dejah") #Crea un objeto de la clase Alumno
-dejah.estudiar() #Llama al metodo estudiar
-dejah.sumar(10,5) #Llama al metodo sumar
+print("Hola mundo Python!!") #Imprime el mensaje
